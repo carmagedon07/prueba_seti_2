688665a8375513fe3b830aaed883cf53001c571f
LAPTOP-PEGSOS31\pcaro <pedro.caro@cafedecolombia.com>
2026-02-08 08:52:01 -0500

    Se incorpora un archivo docker-compose para crear una red privada con la opción de ejecutar el contenedor de MySQL.

--- create-database.sql
+++ create-database.sql
@@ -4,9 +4,7 @@ SELECT 'Base de datos franquicia creada exitosamente' AS resultado;
 -- Este script solo asegura que la base de datos exista
 -- Las tablas se crearán automáticamente al iniciar la aplicación
 
+CREATE DATABASE IF NOT EXISTS franquicia;
 USE franquicia;
 
-CREATE DATABASE IF NOT EXISTS franquicia;
-
 -- Ejecuta este script antes de iniciar la aplicación
-
